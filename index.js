1712731b3835b0c635a5bfb84380bcd566e6da0c
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-09-08 20:26:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+
+    const slackUserName = document.querySelector("[data-testid='slackUserName']");
+    const currentDayOfTheWeek = document.querySelector("[data-testid='currentDayOfTheWeek']");
+    const currentUTCTime = document.querySelector("[data-testid='currentUTCTime']");
+    const slackDisplayImage = document.querySelector("[data-testid='slackDisplayImage']");
+    const track = document.querySelector('[data-testid="myTrack"]')
+    const git = document.querySelector('[data-testid="githubURL"]')
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDate = new Date();
+    const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+    
+    slackUserName.textContent = "Nathan-Yinka";
+    slackDisplayImage.src = "IMG_6602.jpg";
+    slackDisplayImage.alt = "Nathan-Yinka";
+
+    currentDayOfTheWeek.textContent = `Current Day of the Week: ${dayOfWeek}`;
+    git.textContent = "Github Link"
+
+    const currentUTC = currentDate.getTime();
+    currentUTCTime.textContent = `Current UTC Time (Milliseconds): ${currentUTC}`;
+    track.textContent = "Frontend"
+});
